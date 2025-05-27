90ae809c9cf12c3978cb5d67ee1515619df8c6a7
grillitg <gustav.grill@edu.ht-villach.at>
2025-05-27 09:35:03 +0200

    board, winning combos, grid

---
+++ game/src/app/components/Board.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const WINNING_COMBOS = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6],
+];
+
+export default function Board() {
+
+    const [board, setBoard] = useState<string[]>(Array(9).fill(""));
+
+    return (
+        <></>
+    );
+}
