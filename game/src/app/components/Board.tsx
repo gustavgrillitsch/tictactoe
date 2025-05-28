56cae124d9f4260901858c00ff428018f28aa965
grillitg <gustav.grill@edu.ht-villach.at>
2025-05-28 09:32:56 +0200

    Integrated the Square component into the Board to display the game grid. Each square now responds to user clicks and highlights winning combinations, enhancing gameplay functionality.

--- game/src/app/components/Board.tsx
+++ game/src/app/components/Board.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import Square from "./Square";
 
 const WINNING_COMBOS = [
     [0,1,2], [3,4,5], [6,7,8],
@@ -26,6 +27,17 @@ export default function Board() {
 
 
         return (
-        <></>
+        <>
+            <div className="grid grid-cols-3 gap-2">
+                {board.map((value, idx) => (
+                    <Square
+                        key={idx}
+                        value={value}
+                        onClick={() => handleClick(idx)}
+                        highlight={winCombo.includes(idx)}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
